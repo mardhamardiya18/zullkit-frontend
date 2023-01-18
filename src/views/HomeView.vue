13c71d8a560635494bf11e3586afbb9931e12008
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2023-01-18 17:41:07 +0800

    create categories component with props

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,14 @@
-<script setup></script>
+<script setup>
+import CategoriesCard from "./../components/CategoriesCard.vue";
+import { ref } from "vue";
+
+const categories = ref([
+  { id: 1, title: "Mobile UI KIT", count: 15, image: "categories-1.jpg" },
+  { id: 2, title: "Fonts", count: 10, image: "categories-2.jpg" },
+  { id: 3, title: "Icons KIT", count: 15, image: "categories-3.jpg" },
+  { id: 4, title: "Web UI KIT", count: 11, image: "categories-4.jpg" },
+]);
+</script>
 
 <template>
   <main>
@@ -54,114 +64,13 @@
         Top Categories
       </h2>
       <div class="flex flex-wrap -mx-1 lg:-mx-4">
-        <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-          <div class="overflow-hidden border border-gray-200 rounded-xl">
-            <a href="details.html">
-              <div class="m-4 overflow-hidden rounded-xl">
-                <img
-                  alt="Placeholder"
-                  class="block w-full h-auto"
-                  src="@/assets/img/categories-1.jpg"
-                />
-              </div>
-            </a>
-
-            <header class="px-4 mb-4 leading-tight">
-              <h1 class="text-lg">
-                <a
-                  class="font-semibold text-black no-underline hover:underline"
-                  href="#"
-                >
-                  Mobile UI Kit
-                </a>
-              </h1>
-              <span class="block text-sm font-light text-gray-500 no-underline">
-                731 items
-              </span>
-            </header>
-          </div>
-        </div>
-        <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-          <div class="overflow-hidden border border-gray-200 rounded-xl">
-            <a href="details.html">
-              <div class="m-4 overflow-hidden rounded-xl">
-                <img
-                  alt="Placeholder"
-                  class="block w-full h-auto"
-                  src="@/assets/img/categories-2.jpg"
-                />
-              </div>
-            </a>
-
-            <header class="px-4 mb-4 leading-tight">
-              <h1 class="text-lg">
-                <a
-                  class="font-semibold text-black no-underline hover:underline"
-                  href="#"
-                >
-                  Fonts
-                </a>
-              </h1>
-              <span class="block text-sm font-light text-gray-500 no-underline">
-                657 items
-              </span>
-            </header>
-          </div>
-        </div>
-        <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-          <div class="overflow-hidden border border-gray-200 rounded-xl">
-            <a href="details.html">
-              <div class="m-4 overflow-hidden rounded-xl">
-                <img
-                  alt="Placeholder"
-                  class="block w-full h-auto"
-                  src="@/assets/img/categories-3.jpg"
-                />
-              </div>
-            </a>
-
-            <header class="px-4 mb-4 leading-tight">
-              <h1 class="text-lg">
-                <a
-                  class="font-semibold text-black no-underline hover:underline"
-                  href="#"
-                >
-                  Icon Set
-                </a>
-              </h1>
-              <span class="block text-sm font-light text-gray-500 no-underline">
-                83,559 items
-              </span>
-            </header>
-          </div>
-        </div>
-        <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-          <div class="overflow-hidden border border-gray-200 rounded-xl">
-            <a href="details.html">
-              <div class="m-4 overflow-hidden rounded-xl">
-                <img
-                  alt="Placeholder"
-                  class="block w-full h-auto"
-                  src="@/assets/img/categories-4.jpg"
-                />
-              </div>
-            </a>
-
-            <header class="px-4 mb-4 leading-tight">
-              <h1 class="text-lg">
-                <a
-                  class="font-semibold text-black no-underline hover:underline"
-                  href="#"
-                >
-                  Website UI Kit
-                </a>
-              </h1>
-              <span class="block text-sm font-light text-gray-500 no-underline">
-                4,500 items
-              </span>
-            </header>
-          </div>
-        </div>
+        <CategoriesCard
+          v-for="category in categories"
+          :key="category.id"
+          :title="category.title"
+          :count="category.count"
+          :image="category.image"
+        />
       </div>
     </div>
 
