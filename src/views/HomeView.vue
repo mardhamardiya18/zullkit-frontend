7069caa3198435cc45b88c80e6cb5acbeb648a30
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2023-01-18 22:11:49 +0800

    create Item component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import CategoriesCard from "./../components/CategoriesCard.vue";
+import ItemsCard from "./../components/ItemsCard.vue";
 import { ref } from "vue";
 
 const categories = ref([
@@ -7,6 +8,22 @@ const categories = ref([
   { id: 2, title: "Fonts", count: 10, image: "categories-2.jpg" },
   { id: 3, title: "Icons KIT", count: 15, image: "categories-3.jpg" },
   { id: 4, title: "Web UI KIT", count: 11, image: "categories-4.jpg" },
+]);
+
+const items = ref([
+  {
+    id: 1,
+    name: "Mobile UI KIT",
+    category: "Mobile UI KIT",
+    image: "items-1.jpg",
+  },
+  { id: 2, name: "Modern Fonts", category: "Fonts", image: "items-2.jpg" },
+  {
+    id: 3,
+    name: "Website Design",
+    category: "Web UI KIT",
+    image: "items-3.jpg",
+  },
 ]);
 </script>
 
@@ -77,87 +94,13 @@ const categories = ref([
     <div class="container px-4 mx-auto my-16 md:px-12">
       <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
       <div class="flex flex-wrap -mx-1 lg:-mx-4">
-        <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-          <div class="overflow-hidden border border-gray-200 rounded-xl">
-            <a href="details.html">
-              <div class="m-4 overflow-hidden rounded-xl">
-                <img
-                  alt="Placeholder"
-                  class="block w-full h-auto"
-                  src="@/assets/img/items-1.jpg"
-                />
-              </div>
-            </a>
-
-            <header class="px-4 mb-4 leading-tight">
-              <h1 class="text-lg">
-                <a
-                  class="font-semibold text-black no-underline hover:underline"
-                  href="#"
-                >
-                  Mobile UI Kit
-                </a>
-              </h1>
-              <span class="block text-sm font-light text-gray-500 no-underline">
-                Mobile UI Kit
-              </span>
-            </header>
-          </div>
-        </div>
-        <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-          <div class="overflow-hidden border border-gray-200 rounded-xl">
-            <a href="details.html">
-              <div class="m-4 overflow-hidden rounded-xl">
-                <img
-                  alt="Placeholder"
-                  class="block w-full h-auto"
-                  src="@/assets/img/items-2.jpg"
-                />
-              </div>
-            </a>
-
-            <header class="px-4 mb-4 leading-tight">
-              <h1 class="text-lg">
-                <a
-                  class="font-semibold text-black no-underline hover:underline"
-                  href="#"
-                >
-                  Online Doctor Consultation
-                </a>
-              </h1>
-              <span class="block text-sm font-light text-gray-500 no-underline">
-                Website UI Kit
-              </span>
-            </header>
-          </div>
-        </div>
-        <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-          <div class="overflow-hidden border border-gray-200 rounded-xl">
-            <a href="details.html">
-              <div class="m-4 overflow-hidden rounded-xl">
-                <img
-                  alt="Placeholder"
-                  class="block w-full h-auto"
-                  src="@/assets/img/items-3.jpg"
-                />
-              </div>
-            </a>
-
-            <header class="px-4 mb-4 leading-tight">
-              <h1 class="text-lg">
-                <a
-                  class="font-semibold text-black no-underline hover:underline"
-                  href="#"
-                >
-                  Banking Crypto
-                </a>
-              </h1>
-              <span class="block text-sm font-light text-gray-500 no-underline">
-                Mobile UI Kit
-              </span>
-            </header>
-          </div>
-        </div>
+        <ItemsCard
+          v-for="item in items"
+          :key="item.id"
+          :name="item.name"
+          :category="item.category"
+          :image="item.image"
+        />
       </div>
     </div>
   </main>
