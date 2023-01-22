534f202ac27ccba0a89e2ce2a2c22e5f8dc5d261
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2023-01-23 01:10:48 +0800

    fix login & register template-vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +11,24 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/categories',
+      name: 'categories',
+    },
+    {
+      path: '/pricing',
+      name: 'pricing',
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    }
     
   ]
 })
